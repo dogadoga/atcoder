b7757be5e49320b2ee4d4a3574fbd2fc620e37e0
niusankin <kishiouaruhureddo@gmail.com>
2022-04-25 00:44:38 +0900

    abc248

--- abc248/a/main.py
+++ abc248/a/main.py
@@ -2,5 +2,13 @@ def main():
 	import sys
 	input = sys.stdin.readline
 
+	s = input()
+	ans = ""
+	for i in range(0,10):
+		a = str(i)
+		if not a in s:
+			print(a)
+			break
+
 if __name__ == '__main__':
   main()
