de7e5754071e42782dccfc3f7cc94512de6d803c
dogadoga <dogadogasan@gmail.com>
2022-04-29 01:37:17 +0900

    abs081b

--- abs/abc081b/main.py
+++ abs/abc081b/main.py
@@ -7,6 +7,21 @@ def S(): return input().rstrip()
 def LS(): return list(input().rstrip())
 
 def main():
-	
+  N = I()
+  A = LI()
+  flag = True
+  count = 0
+
+  while flag:
+      for i, a in enumerate(A):
+          if a % 2 != 0:
+              flag = False
+              break
+          else:
+              A[i] = a / 2
+      if flag:
+          count += 1
+  print(count)
+  
 if __name__ == '__main__':
   main()
