5196c506fc9c5f678429c6d00ecd087e0bd0688e
dogadoga <dogadogasan@gmail.com>
2022-04-29 01:59:05 +0900

    abc087b

--- abs/abc087b/main.py
+++ abs/abc087b/main.py
@@ -5,3 +5,21 @@ def MI(): return map(int, input().rstrip().split())
 def LI(): return list(map(int,input().rstrip().split()))
 def S(): return input().rstrip()
 def LS(): return list(input().rstrip())
+
+def main():
+	a = I()
+	b = I()
+	c = I()
+	x = I()
+	count = 0
+
+	for i in range(a+1):
+		for j in range(b+1):
+			for k in range(c+1):
+				if i*500 + j*100 + k*50 == x:
+					count += 1
+
+	print(count)
+
+if __name__ == "__main__":
+	main()
